a7807fb6e8bba194f9f28221618ff53a45ae5c12
Andreas Rammhold <andreas@rammhold.de>
2016-08-11 20:10:19 +0200

    Initial commit

---
+++ isc_dhcp_filter/__init__.py
@@ -0,0 +1,123 @@
+from isc_dhcp_leases import Lease
+from isc_dhcp_leases import Lease6
+from isc_dhcp_leases import IscDhcpLeases
+
+def parse(*files):
+
+    return Leases(*[leases for leases in map(lambda file: IscDhcpLeases(file).get(), files)])
+
+
+class Leases:
+    _leases = []
+    _iter = None
+    _current_iterator = None
+
+    def __init__(self, *args):
+        if len(args) > 1:
+            # create a new iterator that can be called again
+            def _iter():
+                for argument in args:
+                    yield from argument
+
+            self._leases = None
+            self._iter = _iter
+        else:
+            self._leases = args[0]
+
+    @property
+    def current(self):
+        """
+        Leases where the active and valid properties are True
+        :return:
+        """
+        return self.active.valid
+
+    @property
+    def active(self):
+        """
+        Leases where the active property is True
+        :return:
+        """
+        g = (l for l in self if l.active)
+        return Leases(g)
+
+    @property
+    def inactive(self):
+        """
+        Leases where the active property is False
+        :return:
+        """
+        g = (l for l in self if not l.active)
+        return Leases(g)
+
+    @property
+    def valid(self):
+        """
+        Leases where the valid property is True
+        :return:
+        """
+        g = (l for l in self if l.valid)
+        return Leases(g)
+
+    @property
+    def invalid(self):
+        """
+        Leases where the valid property is False
+        :return:
+        """
+        g = (l for l in self if not l.valid)
+        return Leases(g)
+
+    @property
+    def v4(self):
+        """
+        Leases that are an instance of Lease
+        :return:
+        """
+        g = (l for l in self if isinstance(l, Lease))
+        return Leases(g)
+
+    @property
+    def v6(self):
+        """
+        Leases that are an instance of Lease6
+        :return:
+        """
+        g = (l for l in self if isinstance(l, Lease6))
+        return Leases(g)
+
+    def filter(self, filter_func):
+        """
+        Filter leases by supplied filter function
+        :param filter_func: function that returns true for all values that should be accepted
+        :return:
+        """
+        g = (l for l in self if filter_func(l))
+        return Leases(g)
+
+    def where_set(self, key, value=None):
+        """
+        Filter leases by supplied set-key and value (if provided)
+        :param key: the key in the sets dictionary of the lease
+        :param value: the value for the supplied key, if None any lease with the key is returned
+        :return:
+        """
+        if value:
+            g = (l for l in self if l.sets.get(key, None) == value)
+        else:
+            g = (l for l in self if key in l.sets)
+
+        return Leases(g)
+
+
+    def __iter__(self):
+        """
+        Returns an iterator for the current set of leases
+        :return:
+        """
+        if self._leases:
+            yield from iter(self._leases)
+        elif self._iter:
+            yield from self._iter()
+        else:
+            raise RuntimeError("Missing _leases and _iter.")
---
+++ tests/__init__.py
@@ -0,0 +1,110 @@
+import os
+from unittest import TestCase
+
+from isc_dhcp_leases import Lease
+from isc_dhcp_leases.iscdhcpleases import BaseLease
+
+from isc_dhcp_filter import parse
+from freezegun import freeze_time
+
+class LeaseLoaderMixin:
+
+    @classmethod
+    def setUpClass(cls):
+        cls._basepath = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'files')
+
+    def setUp(self):
+        self.leases = parse(os.path.join(self._basepath, self.filename))
+        super(LeaseLoaderMixin, self).setUp()
+
+class BaseLeaseTester:
+    def test_active_valid_current(self):
+        active_valid = list(self.leases.active.valid)
+        valid_active = list(self.leases.valid.active)
+
+        self.assertEqual(len(active_valid), len(valid_active))
+        self.assertEqual(len(active_valid), len(list(self.leases.current)))
+
+    def test_v4_filter(self):
+        for lease in self.leases.v4:
+            self.assertIsInstance(lease, Lease)
+
+    def test_v6_filter(self):
+        for lease in self.leases.v4:
+            self.assertIsInstance(lease, Lease)
+
+    def test_valid_filter(self):
+        for lease in self.leases.valid:
+            self.assert_(lease.valid)
+
+    def test_invalid_filter(self):
+        for lease in self.leases.invalid:
+            self.assert_(not lease.valid)
+
+    def test_active_filter(self):
+        for lease in self.leases.active:
+            self.assert_(lease.active)
+
+    def test_inactive_filter(self):
+        for lease in self.leases.inactive:
+            self.assert_(not lease.active)
+
+    def test_current_filter(self):
+        for lease in self.leases.current:
+            self.assert_(lease.active)
+            self.assert_(lease.current)
+
+    def test_filter_func(self):
+        l = list(self.leases.filter(lambda x: False))
+        self.assertEqual(l, [])
+
+        l = list(self.leases.filter(lambda x: isinstance(x, BaseLease)))
+        self.assertEqual(l, list(self.leases))
+
+
+class TestDhcpd6(LeaseLoaderMixin, BaseLeaseTester, TestCase):
+    filename = 'dhcpd6-4.3.3.leases'
+
+    def test_dhcpv6_active(self):
+        leases = self.leases
+
+        self.assertEqual(len(list(leases)), 4)
+        self.assertEqual(len(list(leases.active)), 4)
+
+    @freeze_time("2015-07-6 8:15:0")
+    def test_dhcpv6_active_valid(self):
+        leases = self.leases
+
+        active_valid = list(leases.active.valid)
+        valid_active = list(leases.valid.active)
+
+        self.assertEqual(len(active_valid), len(valid_active))
+        self.assertEqual(len(active_valid), len(list(leases.current)))
+
+    def test_dhcpv6_invalid(self):
+        leases = self.leases
+
+        self.assertEqual(list(leases.invalid), list(leases))
+        self.assertEqual(len(list(leases)), 4)
+
+    def test_where_set(self):
+        leases = self.leases.where_set('iana')
+        self.assertEqual(len(list(leases)), 2)
+
+        leases = self.leases.where_set('iana', '2001:10:30:0:0:0:0:1fe')
+        self.assertEqual(len(list(leases)), 1)
+
+        leases = self.leases.where_set('clientduid', '0100011cf710a5002722332b34')
+        self.assertEqual(len(list(leases)), 2)
+
+        leases = self.leases.where_set('clientduid')
+        self.assertEqual(len(list(leases)), 4)
+
+
+class TestDebian7(LeaseLoaderMixin, BaseLeaseTester, TestCase):
+    filename = 'debian7.leases'
+
+    def test_vendor_class_identifier(self):
+        leases1 = list(self.leases.where_set('vendor-class-identifier'))
+        leases2 = list(self.leases.where_set('vendor-class-identifier', 'Some Vendor Identifier'))
+        self.assertEqual(leases1, leases2)
